97e5dd65db4380288af1ba59b69ae1931cefaccb
Balaji Rao <balajirrao@gmail.com>
2020-12-28 16:28:25 +0100

    Work with unlabelled edges

--- src/Dijkstra.hs
+++ src/Dijkstra.hs
@@ -4,17 +4,20 @@
 
 module Dijkstra where
 
-import Control.Monad.State (MonadState (get, put), State, evalState, forM_)
+import Control.Monad (forM, replicateM)
+import Control.Monad.State.Lazy (MonadState (get, put), State, evalState, forM_)
+import Data.Foldable (foldrM)
 import Data.Graph.Inductive.Arbitrary ()
 import Data.Graph.Inductive.Graph (Graph, Node)
 import qualified Data.Graph.Inductive.Graph as FGL
 import Data.Graph.Inductive.PatriciaTree (Gr)
 import Data.Graph.Inductive.Query.SP (LRTree, spTree)
-import Data.List (foldl', sortBy)
+import Data.List (foldl', nub, sortBy)
 import qualified Data.Map as M
 import Data.Maybe (catMaybes)
 import Data.Ord (comparing)
 import qualified Data.Set as S
+import Debug.Trace
 import GHC.Natural (Natural, intToNatural)
 import Test.QuickCheck (Arbitrary (arbitrary), quickCheck, suchThat, verboseCheck)
 
@@ -22,54 +25,77 @@ data DijkstraState = DijkstraState
   { unvisitedNodes :: S.Set Node,
     tentativeDistances :: M.Map Node Int
   }
+  deriving (Show)
 
 setTentativeDistance :: Node -> Int -> State DijkstraState ()
 setTentativeDistance n d = do
   st@DijkstraState {tentativeDistances} <- get
-  put $ st {tentativeDistances = M.insert n d tentativeDistances}
+  let st' = st {tentativeDistances = M.insert n d tentativeDistances}
+  put st'
 
 selectNextNode :: State DijkstraState (Maybe (Node, Int))
 selectNextNode = do
   st@DijkstraState {..} <- get
-
   let univisitedTentativeDistances =
         M.toList $ M.filterWithKey (\node _ -> node `S.member` unvisitedNodes) tentativeDistances
 
-  case sortBy (comparing snd) univisitedTentativeDistances of
+  let res = sortBy (comparing snd) univisitedTentativeDistances
+  case res of
     [] -> pure Nothing
     (x : _) -> pure $ pure x
 
-doDijkstra :: Graph gr => gr a b -> Node -> Int -> State DijkstraState Int
+markNodeVisited :: Graph gr => gr a b -> Node -> State DijkstraState ()
+markNodeVisited graph node = do
+  st@DijkstraState {unvisitedNodes} <- get
+  let newUnivisitedNodes = S.delete node unvisitedNodes
+  put $ st {unvisitedNodes = newUnivisitedNodes}
+
+doDijkstra :: Graph gr => gr a b -> Node -> Int -> State DijkstraState ()
 doDijkstra graph node nodeTentativeDistance = do
   st@DijkstraState {..} <- get
-  let unvisitedNeighbors = S.intersection unvisitedNodes (S.fromList $ FGL.neighbors graph node)
+  let unvisitedNeighbors = S.intersection unvisitedNodes (S.fromList $ FGL.suc graph node)
 
   forM_ unvisitedNeighbors $ \nei -> do
     let td = M.lookup nei tentativeDistances
         newTd = maybe (1 + nodeTentativeDistance) (min (1 + nodeTentativeDistance)) td
     setTentativeDistance nei newTd
 
-  let newUnivisitedNodes = S.delete node unvisitedNodes
-  put $ st {unvisitedNodes = newUnivisitedNodes}
+  markNodeVisited graph node
 
-  next <- selectNextNode
-  case next of
-    Nothing -> pure nodeTentativeDistance
-    (Just (n', d')) -> doDijkstra graph n' d'
+  let loop = do
+        next <- selectNextNode
+        case next of
+          Nothing -> pure ()
+          (Just (n', d')) -> doDijkstra graph n' d' >> loop
 
-dijkstra :: Graph gr => gr a b -> Node -> Int
-dijkstra graph node = evalState (doDijkstra graph node 0) (DijkstraState mempty mempty)
+  loop
+
+reconstructedSPTree :: Graph gr => gr a b -> Node -> Int -> State DijkstraState [[Node]]
+reconstructedSPTree graph node dist = do
+  st@DijkstraState {..} <- get
+  let sucNodes = S.fromList $ FGL.suc graph node
+  let nextNodes = M.keys $ M.filterWithKey (\n d -> d == dist + 1 && n `S.member` sucNodes) tentativeDistances
+  subTrees <- concat <$> forM nextNodes (\n -> reconstructedSPTree graph n (dist + 1))
+  pure $ ([node] :) $ (node :) <$> subTrees
+
+dijkstra :: Graph gr => gr a b -> Node -> [[Node]]
+dijkstra graph node
+  | node `elem` FGL.nodes graph =
+    reverse
+      <$> evalState
+        (doDijkstra graph node 0 >> reconstructedSPTree graph node 0)
+        (DijkstraState (S.fromList $ FGL.nodes graph) (M.singleton node 0))
+  | otherwise = []
+
+dijkstraGr :: Gr () Natural -> Node -> [[Node]]
+dijkstraGr = dijkstra @Gr
 
 prop_shortestPath :: Graph gr => gr () Natural -> Bool
-prop_shortestPath graph = dijkstra graph node == processSpTree (spTree node graph)
+prop_shortestPath graph = S.fromList (dijkstra graph node) == S.fromList (processSpTree (spTree node graph))
   where
     node = foldr const 0 (FGL.nodes graph)
-    processSpTree :: LRTree a -> Int
-    processSpTree paths =
-      let pathLengths = length . FGL.unLPath <$> paths
-       in case pathLengths of
-            [] -> 0
-            (x : xs) -> foldl' min x xs - 1
+    processSpTree :: LRTree a -> [[Node]]
+    processSpTree paths = nub $ (fst <$>) <$> (FGL.unLPath <$> paths)
 
 instance Arbitrary Natural where
   arbitrary = intToNatural <$> (arbitrary `suchThat` (>= 0))
